24e3aecfc54a3e8066b37eddf413836b230a9a37
Sirajul Islam Sojib <sirajulislam1123@gmail.com>
2019-03-20 20:25:35 +0600

    _taking date and time from EditText

--- app/src/main/java/com/example/sirajulislamsojib/alarmmanagerimplementation/MainActivity.java
+++ app/src/main/java/com/example/sirajulislamsojib/alarmmanagerimplementation/MainActivity.java
@@ -7,11 +7,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
 
     public Context getContext() {
         return getApplicationContext();
@@ -21,29 +26,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText_id);
+
     }
 
     public void startAlarm(View view) {
         Intent intent = new Intent(this,MyAlarmService.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(),
-                101,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+                111,intent,PendingIntent.FLAG_UPDATE_CURRENT);
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 20);
-        calendar.set(Calendar.MINUTE, 59);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        try {
+            Date customDate = formatter.parse(editText.getText().toString());
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(customDate);
 
-        alarmManager.setWindow(AlarmManager.RTC,calendar.getTimeInMillis(),40000,pendingIntent);
+            Calendar newCalendar = Calendar.getInstance();
+            newCalendar.set(
+                    calendar.get(calendar.YEAR),
+                    calendar.get(calendar.MONTH),
+                    calendar.get(calendar.DAY_OF_MONTH),
+                    calendar.get(calendar.HOUR_OF_DAY),
+                    calendar.get(calendar.MINUTE),
+                    calendar.get(calendar.SECOND)
 
-        Toast.makeText(this, "Alarm is set!", Toast.LENGTH_SHORT).show();
+            );
 
+            alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+            Toast.makeText(this, "Alarm is set!", Toast.LENGTH_SHORT).show();
+        } catch (ParseException e) {
+            Toast.makeText(this, "Problem to set Alarm!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void stoptAlarm(View view) {
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this,MyAlarmService.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,101,
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this,111,
                 intent,PendingIntent.FLAG_UPDATE_CURRENT);
         alarmManager.cancel(pendingIntent);
         Toast.makeText(this, "Alarm is stopped!", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/sirajulislamsojib/alarmmanagerimplementation/MyAlarmService.java
+++ app/src/main/java/com/example/sirajulislamsojib/alarmmanagerimplementation/MyAlarmService.java
@@ -1,14 +1,41 @@
 package com.example.sirajulislamsojib.alarmmanagerimplementation;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
 
 public class MyAlarmService extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Alarm Is Running!", Toast.LENGTH_LONG).show();
+        String channelId = "test_notification";
+        String channelName = "testing";
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(channelId,channelName,
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("AlarmManager Working Test");
+            NotificationManager manager = context.getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder notiBuilder = new NotificationCompat.Builder(context,channelId)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("AlarmManger Checking")
+                .setContentText("Alarm is working properly")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(1,notiBuilder.build());
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, Settings.System.DEFAULT_NOTIFICATION_URI);
+        mediaPlayer.start();
+
     }
 
 }
